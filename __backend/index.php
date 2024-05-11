2daf1e2c28421c17c72fa6022c22ec036dcc63e0
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-11 14:54:44 +0700

    create __backend layout

---
+++ __backend/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="title">
+            <h1>Admin Page</h1>
+        </div>
+        <div class="profile">
+            <p>Welcome, Admin</p>
+        </div>
+        <div class="nav-list">
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="">Data User</a></li>
+                <li><a href="">Data Menu</a></li>
+                <li><a href="">Data Order</a></li>
+            </ul>
+        </div>
+        <a href="" class="logout">Logout</a>
+    </div>
+    <div class="main-page">
+        <div class="header">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="content">
+            <p>Selamat datang di halaman admin. Silahkan pilih menu yang tersedia di sebelah kiri untuk mengelola data user, data menu, dan data order.</p>
+        </div>
+    </div>
+</body>
+
+</html>
