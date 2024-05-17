e9c1d4c2e74569438174481be77a124338411d6c
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-17 17:57:10 +0700

    update function

--- function.php
+++ function.php
@@ -2,6 +2,12 @@
 
 session_start();
 
+$error = [
+    'error' => '',
+    'pesan' => ''
+];
+
+
 function koneksi()
 {
     return mysqli_connect("localhost", "root", "", "pw2024_tubes_233040028");
@@ -31,82 +37,83 @@ function login($data)
     $username = htmlspecialchars($data['username']);
     $password = htmlspecialchars($data['password']);
 
-    if ($user = query("SELECT * FROM user WHERE username = '$username'")) {
-        if ($user['password'] === $password) {
+    if ($user = query("SELECT * FROM user WHERE username = '$username'")) :
+        if (password_verify($password, $user['password'])) :
             $_SESSION['login'] = true;
             $_SESSION['id'] = $user['id'];
             header("Location: ../index.php");
             exit;
-        }
-    }
-
-    // if ($user = query("SELECT * FROM user WHERE username = '$username'")) {
-    //     if (password_verify($password, $user['password'])) {
-    //         $_SESSION['login'] = true;
-    //         $_SESSION['id'] = $user['id'];
-    //         header("Location: index.php");
-    //         exit;
-    //     }
-    // }
-    return [
-        'error' => true,
-        'pesan' => 'Username / Password Salah!'
-    ];
+        else :
+            echo "<script>
+    alert('password salah!');
+    document.location.href = 'login.php';
+        </script>";
+        // $error['error'] = true;
+        // $error['pesan'] = 'Password Salah!';
+        endif;
+    else :
+        echo "<script>
+    alert('username tidak ada!');
+    document.location.href = 'login.php';
+        </script>";
+    // $error['error'] = true;
+    // $error['pesan'] = 'Username Tidak Ditemukan!';
+    endif;
 }
 
-function registrasi($data)
+function register($data)
 {
     $conn = koneksi();
 
     $username = htmlspecialchars(strtolower($data['username']));
     $email = htmlspecialchars(strtolower($data['email']));
-    $password1 = mysqli_real_escape_string($conn, $data['password1']);
-    $password2 = mysqli_real_escape_string($conn, $data['password2']);
-    $gambar = ($data['gambar']);
+    $password = mysqli_real_escape_string($conn, $data['password']);
+    $cPassword = mysqli_real_escape_string($conn, $data['cPassword']);
+    // $gambar = ($data['gambar']);
     $role = ($data['id_role']);
-
-
     // jika password atau apapun kosong
-    if (empty($username)  || empty($password1) ||  empty($password2) || empty($email)) {
+    if (empty($username) or empty($password) or  empty($cPassword) or empty($email)) {
         echo "<script>
       alert('Tolong isi semuanya');
-      document.location.href = 'registrasi.php';
+      document.location.href = 'register.php';
       </script>";
         return false;
     }
-
-
     // jika username sudah ada
     if (query("SELECT * FROM user WHERE username = '$username'")) {
         echo "<script>
     alert('username atau passsword sudah ada');
-    document.location.href = 'registrasi.php';
+    document.location.href = 'register.php';
     </script>";
         return false;
     }
     // konfirmasi password tidak sesuai
-    if ($password1 !== $password2) {
+    if ($password !== $cPassword) {
         echo "<script>
       alert('Password tidak sesuai');
-      document.location.href = 'registrasi.php';
+      document.location.href = 'register.php';
       </script>";
     }
     // jika password < 5 digit strlen untuk menghitung panjang string
-    if (strlen($password1) < 5) {
+    if (strlen($password) < 5) {
         echo "<script>
               alert('password terlalu pendek!');
-              document.location.href = 'registrasi.php';
+              document.location.href = 'register.php';
             </script>";
         return false;
     }
     // jika username & password sudah sesuai
     // enkripsi password
-    $password_baru = password_hash($password1, PASSWORD_DEFAULT);
+    $password_baru = password_hash($password, PASSWORD_DEFAULT);
     // insert ke tabel user
     $query = "INSERT INTO user
                 VALUES
-              (NULL, '$gambar', '$username', '$email', '$password_baru', '$role'  )
+              (NULL, '', '$username', '$email', '$password_baru', '$role'  )
             ";
+    echo "<script>
+    alert('Akun berhasil dibuat!');
+    document.location.href = 'login.php';
+        </script>";
     mysqli_query($conn, $query) or die(mysqli_error($conn));
     return mysqli_affected_rows($conn);
 }
