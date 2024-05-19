bb167621de0c07c49508fbdcf546c8b339f6c008
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-19 20:09:55 +0700

    create users, add users

---
+++ __backend/users/add_user.php
@@ -0,0 +1,43 @@
+<?php $title = 'Add User' ?>
+
+<form action="" method="POST">
+    <div class="flex">
+        <div class="col">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </div>
+    </div>
+    <button type="submit" name="login" class="btn">Login</button>
+</form>
---
+++ __backend/users/users.php
@@ -0,0 +1,37 @@
+<div class="add-data">
+    <a href="index.php?page=add_user">Add Data</a>
+</div>
+<table class="table">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>admin</td>
+            <td>admin@mail.com</td>
+            <td>admin</td>
+            <td>delete</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>admin</td>
+            <td>admin@mail.com</td>
+            <td>admin</td>
+            <td>delete</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>admin</td>
+            <td>admin@mail.com</td>
+            <td>admin</td>
+            <td>delete</td>
+        </tr>
+    </tbody>
+</table>
