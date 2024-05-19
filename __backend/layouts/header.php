8fd3d94f705ec5c0aaeabfd0b2573958fa1ddee1
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-19 20:08:38 +0700

    create layout for backend

---
+++ __backend/layouts/header.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="topbar">
+        <h3>Admin Page</h3>
+    </div>
+</header>
+<div class="wrap">
+    <div class="sidebar">
+        <div class="nav-list">
+            <ul>
+                <li><a href="index.php?page=index">Dashboard</a></li>
+                <li><a href="index.php?page=users">Data User</a></li>
+                <li><a href="">Data Menu</a></li>
+                <li><a href="">Data Order</a></li>
+            </ul>
+        </div>
+        <a href="" class="logout">Logout</a>
+        <div class="nav-toggle">
+            <button>
+                <p>X</p>
+            </button>
+        </div>
+    </div>
+    <div class="wrap-content">
+        <div class="content">
+            <div class="header-text">
+                <h1>Dashboard</h1>
+            </div>
