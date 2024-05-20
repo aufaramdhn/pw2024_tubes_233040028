b58d63608f1dff2af3fae617816f61c67dd3f35e
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-20 21:23:40 +0700

    test function delete data

--- __backend/users/users.php
+++ __backend/users/users.php
@@ -1,37 +1,56 @@
+<?php
+
+
+$users = query("SELECT * FROM user");
+
+if (isset($_GET['id'])) :
+    delete_data('user', ['id' => $_GET['id']]);
+endif;
+
+?>
+
 <div class="add-data">
     <a href="index.php?page=add_user">Add Data</a>
 </div>
+
+<?php
+
+if (isset($_SESSION['message'])) :
+
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+
+endif;
+
+?>
+
 <table class="table">
     <thead>
         <tr>
             <th>No</th>
             <th>Username</th>
+            <th>Password</th>
             <th>Email</th>
             <th>Role</th>
             <th>Action</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>1</td>
-            <td>admin</td>
-            <td>admin@mail.com</td>
-            <td>admin</td>
-            <td>delete</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>admin</td>
-            <td>admin@mail.com</td>
-            <td>admin</td>
-            <td>delete</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>admin</td>
-            <td>admin@mail.com</td>
-            <td>admin</td>
-            <td>delete</td>
-        </tr>
+        <?php
+        $id = 1;
+        foreach ($users as $user) :
+        ?>
+            <tr>
+                <td><?= $id++ ?></td>
+                <td><?= $user['username'] ?></td>
+                <td><?= $user['password'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td><?= $user['id_role'] ?></td>
+                <td>
+                    <a href="index.php?page=edit_user&id=<?= $user['id'] ?>">Edit</a>
+                    <a href="index.php?page=users&id=<?= $user['id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
     </tbody>
 </table>
