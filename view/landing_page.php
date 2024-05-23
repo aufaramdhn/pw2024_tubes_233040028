40335d963b6cc25a30e3684d7e1d2939e4394ea2
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-23 22:07:34 +0700

    update landing page

---
+++ view/landing_page.php
@@ -0,0 +1,91 @@
+<section id="home" class="container">
+    <div class="wrap-home">
+        <div class="deskripsi-home">
+            <h1>Selamat Datang di [Nama Situs]: Solusi Pemesanan Makanan yang Praktis dan Lezat!</h1>
+            <p>Destinasi online Anda untuk memesan makanan favorit Anda dengan mudah dan cepat. Dengan kami, Anda tidak perlu lagi repot-repot keluar rumah atau menelepon restoran, karena semua pesanan Anda dapat diproses hanya dengan beberapa klik!</p>
+            <a href="" class="btn">Pilih Menu</a>
+        </div>
+        <div class="img-home">
+            <img src="/assets/picture/login.jpg" alt="" />
+        </div>
+    </div>
+</section>
+
+<section id="menu-count" class="container">
+    <div class="wrap-menu-count">
+        <div class="food-menu">
+            <h3>3+</h3>
+            <p>Menu Makanan</p>
+        </div>
+        <!-- <div class="vertical-line"></div> -->
+        <div class="drink-menu">
+            <h3>3+</h3>
+            <p>Menu Minuman</p>
+        </div>
+        <!-- <div class="vertical-line"></div> -->
+        <div class="experience">
+            <h3>3+</h3>
+            <p>Pengalaman Pelayanan</p>
+        </div>
+    </div>
+</section>
+
+<section id="about" class="container">
+    <div class="wrap-about">
+        <div class="photo-about">
+            <img src="https://source.unsplash.com/400x400/?nature,water" alt="" />
+        </div>
+        <div class="deskripsi-about">
+            <h1>About Us</h1>
+            <p>FOOD adalah layanan pemesanan makanan online yang menyediakan berbagai macam menu makanan lezat dan bergizi. Kami berkomitmen untuk memberikan layanan terbaik bagi pelanggan kami, dengan menyediakan makanan yang berkual</p>
+        </div>
+</section>
+
+<section id="menu" class="container">
+    <div class="header-text">
+        <h1>Our Menus</h1>
+        <p>Pilih menu makanan favorit Anda dan nikmati makanan lezat yang kami sediakan!</p>
+    </div>
+    <div class="wrap-menu">
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+    </div>
+</section>
