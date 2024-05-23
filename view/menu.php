4c944db89fb4d3f21842322fb405b687ee9a50c4
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-23 22:07:44 +0700

    create layout menu

--- view/menu.php
+++ view/menu.php
@@ -1,95 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
-
-<body>
-    <header class="header-nav">
-        <div class="container">
-            <div class="logo">
-                <h1>FOOD</h1>
+<section id="menu" class="container">
+    <div class="header-text">
+        <h1>Menu Kami</h1>
+        <p>Pilih menu makanan favorit Anda dan nikmati makanan lezat yang kami sediakan!</p>
+    </div>
+    <div class="search-menu">
+        <input id="search" type="text" placeholder="Cari menu makanan">
+        <label for="search"><i class="ri-search-line"></i></label>
+        <button><i class="ri-filter-line"></i></button>
+        <div class="filter">
+            <ul>
+                <li><a href="">All</a></li>
+                <li><a href="">Breakfast</a></li>
+                <li><a href="">Lunch</a></li>
+                <li><a href="">Dinner</a></li>
+                <li><a href="">Dessert</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="wrap-menu">
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
             </div>
-            <nav>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </nav>
-            <div class="profile">
-                <div class="toggle-dropdown">
-                    <button>X</button>
-                </div>
-                <?php if (isset($_SESSION['login'])) :  ?>
-                    <div class="dropdown">
-                        <div class="dropdown-content">
-                            <a href="">Logout</a>
-                            <a href="">Logout</a>
-                            <a href="">Logout</a>
-                            </ul>
-                        </div>
-                    </div>
-                <?php else : ?>
-                    <a href="auth/login.php">Login</a>
-                <?php endif ?>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
             </div>
         </div>
-    </header>
-    <section id="menu" class="container">
-        <div class="header-text">
-            <h1>Menu Kami</h1>
-            <p>Pilih menu makanan favorit Anda dan nikmati makanan lezat yang kami sediakan!</p>
-        </div>
-        <div class="wrap-menu">
-            <div class="card">
-                <div class="card-img">
-                    <img src="https://source.unsplash.com/300x300/?food" alt="" />
-                </div>
-                <div class="card-content">
-                    <h3>Menu 1</h3>
-                    <p>$13</p>
-                    <a href="" class="btn">Pesan</a>
-                </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
             </div>
-            <div class="card">
-                <div class="card-img">
-                    <img src="https://source.unsplash.com/300x300/?food" alt="" />
-                </div>
-                <div class="card-content">
-                    <h3>Menu 1</h3>
-                    <p>$13</p>
-                    <a href="" class="btn">Pesan</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-img">
-                    <img src="https://source.unsplash.com/300x300/?food" alt="" />
-                </div>
-                <div class="card-content">
-                    <h3>Menu 1</h3>
-                    <p>$13</p>
-                    <a href="" class="btn">Pesan</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-img">
-                    <img src="https://source.unsplash.com/300x300/?food" alt="" />
-                </div>
-                <div class="card-content">
-                    <h3>Menu 1</h3>
-                    <p>$13</p>
-                    <a href="" class="btn">Pesan</a>
-                </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
             </div>
         </div>
-    </section>
-</body>
-
-</html>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://source.unsplash.com/300x300/?food" alt="" />
+            </div>
+            <div class="card-content">
+                <h3>Menu 1</h3>
+                <p>$13</p>
+                <a href="" class="btn">Pesan</a>
+            </div>
+        </div>
+    </div>
+</section>
