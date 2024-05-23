01949352914a504c521d54f03b190001df5ed573
aufaramdhn <ramadhanaufa3@gmail.com>
2024-05-23 22:07:25 +0700

    create cart frontend

---
+++ view/cart.php
@@ -0,0 +1,67 @@
+<section id="cart">
+    <div class="header-text">
+        <h1>Cart</h1>
+        <p>Choose your favorite menu</p>
+    </div>
+
+    <div class="container">
+        <div class="wrap-cart">
+            <div class="wrap-cart-img">
+                <img src="https://source.unsplash.com/200x200/?food" alt="cart">
+            </div>
+            <div class="cart-content">
+                <div class="wrap-cart-text">
+                    <div class="wrap-cart-title">
+                        <h3>Menu 1</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae quis
+                        </p>
+                    </div>
+                    <div class="wrap-cart-price">
+                        <p>$13</p>
+                    </div>
+                    <!-- <h1>Your Cart is Empty</h1>
+                <p>Looks like you haven't added anything to your cart yet</p>
+                <a href="index.php?page=menu">Start Shopping</a> -->
+                </div>
+                <div class="wrap-cart-qty">
+                    <div class="cart-qty">
+                        <button><i class="ri-subtract-line"></i></button>
+                        <span>0</span>
+                        <button><i class="ri-add-line"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap-cart">
+            <div class="wrap-cart-img">
+                <img src="https://source.unsplash.com/200x200/?food" alt="cart">
+            </div>
+            <div class="cart-content">
+                <div class="wrap-cart-text">
+                    <div class="wrap-cart-title">
+                        <h3>Menu 1</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae quis
+                        </p>
+                    </div>
+                    <div class="wrap-cart-price">
+                        <p>$13</p>
+                    </div>
+                    <!-- <h1>Your Cart is Empty</h1>
+                <p>Looks like you haven't added anything to your cart yet</p>
+                <a href="index.php?page=menu">Start Shopping</a> -->
+                </div>
+                <div class="wrap-cart-qty">
+                    <div class="cart-qty">
+                        <button><i class="ri-subtract-line"></i></button>
+                        <span>0</span>
+                        <button><i class="ri-add-line"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="hr-lines"></div> -->
+
+</section>
