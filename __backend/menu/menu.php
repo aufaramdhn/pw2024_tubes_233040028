7a62bb6bfb9e62a8d6c015f9e8e00c553aa040e7
aufaramdhn <ramadhanaufa3@gmail.com>
2024-06-05 21:56:31 +0700

    update table menu

--- __backend/menu/menu.php
+++ __backend/menu/menu.php
@@ -2,22 +2,16 @@
 
 $menus = query("SELECT * FROM menu");
 
+
 ?>
+
+<div class="div">
+    <a href="">Dashboard</a> / Menus
+</div>
 
 <div class="add-data">
     <a href="index.php?page=add_menu">Add Data</a>
 </div>
-
-<?php
-
-if (isset($_SESSION['message'])) :
-
-    echo $_SESSION['message'];
-    unset($_SESSION['message']);
-
-endif;
-
-?>
 
 <table class="table">
     <thead>
@@ -36,7 +30,7 @@ endif;
         ?>
             <tr>
                 <td><?= $id++ ?></td>
-                <td align="center"><img src="assets/upload/<?= $menu['menu_img'] ?>" class="img-preview" width="120" height="120"></td>
+                <td align="center"><img src="<?= base_url("assets/upload/$menu[menu_img]") ?>" class="img-preview" width="120" height="120"></td>
                 <td><?= $menu['menu_name'] ?></td>
                 <td>Rp. <?= number_format($menu['menu_price']) ?></td>
                 <td align="center">
