5678a7d103b5d565a302543a4769c35f98a8d92f
aufaramdhn <ramadhanaufa3@gmail.com>
2024-06-05 21:57:25 +0700

    testing function upload frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,15 @@ function previewImage() {
     imgPreview.src = oFREvent.target.result;
   };
 }
+
+function previewImage() {
+  const image = document.querySelector(".image");
+  const imgPreview = document.querySelector(".img-preview");
+
+  const oFReader = new FileReader();
+  oFReader.readAsDataURL(image.files[0]);
+
+  oFReader.onload = function (oFREvent) {
+    imgPreview.src = oFREvent.target.result;
+  };
+}
